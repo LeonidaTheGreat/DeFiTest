0042d4966e27e2f4df3487d2585863e8ad1cc44e
STOJAN MADJUNKOV <madzunkov@hotmail.com>
2022-08-24 01:18:33 -0400

    Update my project

---
+++ defi-staking-app-starter/build/precache-manifest.912db6ec6340c0d492fa1b79642d4ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd22c46d9d4e72ddecb5",
+    "url": "/static/css/main.abab5297.chunk.css"
+  },
+  {
+    "revision": "cd22c46d9d4e72ddecb5",
+    "url": "/static/js/main.cd22c46d.chunk.js"
+  },
+  {
+    "revision": "e2d7c42eab26ae76bca3",
+    "url": "/static/css/1.483c871d.chunk.css"
+  },
+  {
+    "revision": "e2d7c42eab26ae76bca3",
+    "url": "/static/js/1.e2d7c42e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "de64ee4577c1b49eb28f2d59fd85e5cb",
+    "url": "/index.html"
+  }
+];
